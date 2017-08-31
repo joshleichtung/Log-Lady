46d9a0a7e8b0c50c31ca23e199a7c78591050b1d
Josh Leichtung <joshleichtung@gmail.com>
2017-08-31 13:19:32 -0700

    Implement async-sort - fix promise rejection typeerror

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,5 +1,25 @@
 'use strict'
+const Heap = require('heap')
+const P = require('bluebird')
+const Async = require('async')
 
 module.exports = (logSources, printer) => {
-	throw new Error('Not implemented yet!  That part is up to you!')
+  let logHeap = new Heap((a, b) => a.log.date - b.log.date)
+
+  P.map(logSources, log => log.popAsync())
+    .then(logs => {
+      logs.forEach((log, i) => logHeap.push({sourceIndex: i, log: log}))
+    })
+    .then(() => printAndUpdateLogHeap(logHeap, printer, logSources))
+}
+
+function printAndUpdateLogHeap(logHeap, printer, logSources) {
+  let toPrint = logHeap.pop()
+  printer.print(toPrint.log)
+  logSources[toPrint.sourceIndex].popAsync()
+    .then((nextLog) => {
+      nextLog && logHeap.push({sourceIndex: toPrint.sourceIndex, log: nextLog})
+      (!logHeap.empty() && printAndUpdateLogHeap(logHeap, printer, logSources)) || printer.done()
+    })
+    // .reject((err) => console.log(err))
 }
